d2ce452d1b2597282749f79492b86bf2266a5bc5
intellectape <adi_b09@hotmail.com>
2017-02-04 16:39:55 -0500

    Adding whois python file

---
+++ Networking/whois.py
@@ -0,0 +1,15 @@
+# WHOIS project
+
+from subprocess import call
+import sys
+
+if len(sys.argv) != 2:
+    print("python whois.py <IP Address> ")
+    exit(1)
+
+
+def findwhois(param): 
+    return call(["whois", param])
+
+if __name__ == "__main__":
+    findwhois(sys.argv[1])
